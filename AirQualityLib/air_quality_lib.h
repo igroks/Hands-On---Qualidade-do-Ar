76235624952ede859f1b0e8e16a5b31f0e4f5fb2
igroks <igor.carvalho@icomp.ufam.edu.br>
2025-02-18 19:50:49 -0400

    fix: add const keyword in path dir

--- AirQualityLib/air_quality_lib.h
+++ AirQualityLib/air_quality_lib.h
@@ -99,7 +99,7 @@ class AirQuality {
         AirQualityData sensorData;                                   // Attribute that stores the last reading data
         time_t timestampLastUpdate;                                  // Timestamp of the last reading
         int timestampDelta = 5;                                      // Minimum time in seconds that must pass between readings to update the cache
-        char dirPath[] = "/sys/kernel/airquality/sensor";            // File where the driver writes the sensor data
+        const char dirPath[] = "/sys/kernel/airquality/sensor";            // File where the driver writes the sensor data
 };
 
 } // namespace
