0615c10f095a18f58819b0167e1b36bcf707a73c
Emídio Aranha <emidioaranha21@gmail.com>
2025-02-27 21:10:28 -0400

    8.0(App): Fetching sensor data

--- AirQualityApp2/app/src/main/java/com/example/airqualityapp/MainActivity.kt
+++ AirQualityApp2/app/src/main/java/com/example/airqualityapp/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.airqualityapp
 import android.annotation.SuppressLint
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -32,10 +38,22 @@ import com.example.airqualityapp.ui.theme.AirQualityAppTheme
 import com.example.airqualityapp.utils.getBackgroundGradient
 import org.osmdroid.config.Configuration.getInstance
 
+
 class MainActivity : ComponentActivity() {
+    private lateinit var sensorManager: SensorManager
     @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager;
+        val sensorList = sensorManager.getSensorList(Sensor.TYPE_DEVICE_PRIVATE_BASE)
+
+        val sensor1 = sensorList[0]
+        sensorManager.registerListener(sds011listener, sensor1, SensorManager.SENSOR_DELAY_NORMAL)
+        val sensor2 = sensorList[1]
+        sensorManager.registerListener(mq9listener, sensor2, SensorManager.SENSOR_DELAY_NORMAL)
+        val sensor3 = sensorList[2]
+        sensorManager.registerListener(dht11listener, sensor3, SensorManager.SENSOR_DELAY_NORMAL)
 
         getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
 
@@ -45,6 +63,45 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        sensorManager.unregisterListener(sds011listener)
+        sensorManager.unregisterListener(mq9listener)
+        sensorManager.unregisterListener(dht11listener)
+    }
+
+    private val sds011listener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            // The value of the first subscript in the values array is the current light intensity
+            val value = event.values[0]
+            Log.d("SDS011", "Value: $value")
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        }
+    }
+
+    private val mq9listener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            // The value of the first subscript in the values array is the current light intensity
+            val value = event.values[0]
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        }
+    }
+
+    private val dht11listener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            // The value of the first subscript in the values array is the current light intensity
+            val value = event.values[0]
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        }
+    }
+
 }
 
 @Composable
@@ -58,12 +115,16 @@ fun AirQualityApp() {
     val selectedTabIndex = routes.indexOf(currentRoute).coerceAtLeast(0)
     val backgroundGradient = getBackgroundGradient()
     Scaffold(
-        modifier = Modifier.fillMaxSize().background(backgroundGradient),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundGradient),
         containerColor = Color.Black.copy(alpha = 0.4f), // Transparência acinzentada no fundo do Scaffold
         topBar = {
             if(currentRoute != "splash") TabRow(
                 selectedTabIndex = selectedTabIndex,
-                modifier = Modifier.fillMaxWidth().background(backgroundGradient),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(backgroundGradient),
                 containerColor = Color.Black.copy(alpha = 0.2f), // Transparente e acinzentado no fundo das abas
                 contentColor = Color.White,
                 indicator = { tabPositions ->
